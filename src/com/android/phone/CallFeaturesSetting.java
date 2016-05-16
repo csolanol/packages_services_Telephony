eafa12fba0f0b0e26db9701e1280753178544e37
GWX <kagwind@gmail.com>
2016-05-16 20:54:31 +0200

    [2/3] Flip to Mute/Reject Call
    - code cleanup by Yank555.lu
    
    Call Settings - add menu entry
    
    Conflicts:
    	res/xml/call_feature_setting.xml
    
    Change-Id: I2eef5d9179a7df0b86ca6e47ff247e36543a4506

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -115,6 +115,8 @@ public class CallFeaturesSetting extends PreferenceActivity
 
     private static final String ENABLE_VIDEO_CALLING_KEY = "button_enable_video_calling";
 
+    private static final String FLIP_ACTION_KEY = "flip_action";
+
     private Phone mPhone;
     private SubscriptionInfoHelper mSubscriptionInfoHelper;
     private TelecomManager mTelecomManager;
@@ -125,6 +127,7 @@ public class CallFeaturesSetting extends PreferenceActivity
     private Preference mSdnButton;
     private ConfigResourceUtil mConfigResUtil = new ConfigResourceUtil();
 
+    private ListPreference mFlipAction;
     private SwitchPreference mProxSpeaker;
     private SlimSeekBarPreference mProxSpeakerDelay;
     private SwitchPreference mProxSpeakerIncallOnly;
@@ -198,10 +201,22 @@ public class CallFeaturesSetting extends PreferenceActivity
             int delay = Integer.valueOf((String) objValue);
             Settings.System.putInt(getContentResolver(),
                     Settings.System.PROXIMITY_AUTO_SPEAKER_DELAY, delay);
+        } else if (preference == mFlipAction) {
+            int index = mFlipAction.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getContentResolver(),
+                Settings.System.CALL_FLIP_ACTION_KEY, index);
+            updateFlipActionSummary(index);
         }
 
         // Always let the preference setting proceed.
         return true;
+    }
+
+    private void updateFlipActionSummary(int value) {
+        if (mFlipAction != null) {
+            String[] summaries = getResources().getStringArray(R.array.flip_action_summary_entries);
+            mFlipAction.setSummary(getString(R.string.flip_action_summary, summaries[value]));
+        }
     }
 
     @Override
@@ -244,6 +259,15 @@ public class CallFeaturesSetting extends PreferenceActivity
         }
 
         PreferenceScreen prefSet = getPreferenceScreen();
+
+        mFlipAction = (ListPreference) findPreference(FLIP_ACTION_KEY);
+        if (mFlipAction != null) {
+            int flipAction = Settings.System.getInt(getContentResolver(),
+                    Settings.System.CALL_FLIP_ACTION_KEY, 2);
+            mFlipAction.setValue(String.valueOf(flipAction));
+            updateFlipActionSummary(flipAction);
+            mFlipAction.setOnPreferenceChangeListener(this);
+        }
 
         mProxSpeaker = (SwitchPreference) findPreference(PROX_AUTO_SPEAKER);
         mProxSpeakerIncallOnly = (SwitchPreference) findPreference(PROX_AUTO_SPEAKER_INCALL_ONLY);
